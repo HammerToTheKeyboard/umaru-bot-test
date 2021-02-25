b664ccfe6a02c068b69ff838556b5d91f0b697ee
HammerToTheKeyboard <43912810+HammerToTheKeyboard@users.noreply.github.com>
2021-02-25 21:31:35 +0100

    Add files via upload

--- umaru-bot-test-main/globals.py
+++ umaru-bot-test-main/globals.py
@@ -1,7 +1,5 @@
-import discord 
-def init():
-	global client
-	client = discord.Client()
-	global TOKEN
-	TOKEN = ""
+global TOKEN
+TOKEN = "ODA0MDY4OTc3MTgxNDU4NDQy.YBG9lw.ZsigYw07vk4_4PAF_xKen9tMewc"
+global PREFIX
+PREFIX = "u!"
 	
--- umaru-bot-test-main/main.py
+++ umaru-bot-test-main/main.py
@@ -1,9 +1,37 @@
 import discord
-import globals
+from discord.ext import commands
+from globals import TOKEN, PREFIX
 
-client = globals.client
-client.run(TOKEN)
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix = PREFIX, case_insensitive = True, intents=intents)
+
+# commands
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+
+
+# events
+@client.event
+async def on_member_join(member):
+	print(f"{member.name} has joined")
+	await client.get_channel(768450587909554219).send(f"{member.name} has joined!")
+
+@client.event
+async def on_member_remove(member):
+	print(f'{member.name}has left')
+	await client.get_channel(768450587909554219).send(f"{member.name} has left!")
 
 @client.event
 async def on_ready():
-	print('SUCCESSFULLY LOGGED IN AS {client.user.name.upper}'.format(client))
+	print('SUCCESSFULLY LOGGED IN AS', client.user.name.upper())
+	await client.get_channel(768450587909554219).send(f"{client.user.name} is now online!")
+
+@client.event
+async def on_error(event, args, kwargs):
+	print(event, args, kwargs)
+
+
+client.run(TOKEN)
+
